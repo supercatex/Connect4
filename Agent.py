84845dbbe622b31af1ef63a6489746356945c9ea
Kinda <lamkinun@gmail.com>
2019-10-07 18:19:14 +0800

    MCTS

--- Agent.py
+++ Agent.py
@@ -1,6 +1,8 @@
 #!usr/bin/env python
 import random
 import copy
+from GameBoard import GameBoard
+import numpy as np
 
 
 class Agent(object):
@@ -56,6 +58,10 @@ class TwoStepAgent(OneStepAgent):
 
 
 class NStepAgent(OneStepAgent):
+	def __init__(self, name, player, game, max_depth=3):
+		super(NStepAgent, self).__init__(name, player, game)
+		self.max_depth = max_depth
+
 	def minmax(self, depth, g, alpha=-9999, beta=9999, is_your_turn=True):
 		if depth == 0:
 			return 0, None
@@ -100,7 +106,98 @@ class NStepAgent(OneStepAgent):
 		return best_val, best_choice
 		
 	def get_choice(self):
-		v, c = self.minmax(10, copy.deepcopy(self.game))
+		v, c = self.minmax(self.max_depth, copy.deepcopy(self.game))
 		if c is not None:
 			return c
 		return random.choice(self.game.get_choices())
+
+
+class Action(object):
+
+	def __init__(self, choice, previous_action=None):
+		self.choice = choice
+		self.previous_action = previous_action
+		self.next_actions: Action = []
+		self.simulations = 0
+		self.winners = 0
+
+	def get_next_action(self, choice):
+		for na in self.next_actions:
+			if na.choice == choice:
+				return na
+		return None
+
+	def get_UCT(self):
+		dict = {}
+		t = self.simulations
+		for a in self.next_actions:
+			w = a.winners
+			n = a.simulations
+			if n > 0:
+				uct = w / n + np.sqrt(2 * np.log10(t) / n)
+			else:
+				uct = 0
+			dict[a.choice] = uct
+		return dict
+
+	def make_action(self, choice):
+		if self.get_next_action(choice) is None:
+			self.next_actions.append(Action(choice, self))
+
+	def __str__(self):
+		s = ""
+		for na in self.next_actions:
+			s += str(na.choice) + " "
+		return s
+
+
+class UCTAgent(RandomAgent):
+
+	def __init__(self, name, player, game):
+		super(UCTAgent, self).__init__(name, player, game)
+		self.current_action = Action(0)
+		self.agent = NStepAgent(name, player, game)
+
+	def random_walk(self, depth=0):
+		player = (self.player + depth + 1) % 2 + 1
+		choices = self.game.get_choices()
+		if len(choices) == 0:
+			return 0
+
+		c = random.choice(choices)
+		self.current_action.make_action(c)
+		self.current_action.simulations += 1
+		self.current_action = self.current_action.get_next_action(c)
+
+		self.game.move(c, player)
+		self.game.next_player()
+		# self.game.print_board()
+
+		result = 0
+		if self.game.is_winner(c, player):
+			self.game.moveback()
+			self.game.next_player()
+			if player == self.player:
+				result = 1
+		else:
+			result = self.random_walk(depth + 1)
+			self.game.moveback()
+			self.game.next_player()
+
+		self.current_action = self.current_action.previous_action
+		self.current_action.winners += result
+		return result
+
+	def get_choice(self):
+		# v, c = self.agent.minmax(3, copy.deepcopy(self.game))
+		# if c is not None:
+		# 	self.current_action.make_action(c)
+		# 	self.current_action = self.current_action.get_next_action(c)
+		# 	return c
+
+		for i in range(1000):
+			self.random_walk()
+		uct = self.current_action.get_UCT()
+		choice = max(uct, key=uct.get)
+		self.current_action = self.current_action.get_next_action(choice)
+		return choice
--- main.py
+++ main.py
@@ -8,17 +8,20 @@ import time
 if __name__ == "__main__":
 	p1 = 0
 	p2 = 0
+	is_print = False
 	for i in range(100):
 		g = GameBoard()
-		# g.print_board()
+		if is_print:
+			g.print_board()
 
-		players = [TwoStepAgent("COM1", 1, g), NStepAgent("COM2", 2, g)]
+		players = [UCTAgent("COM1", 1, g), NStepAgent("COM2", 2, g, 10)]
 		
 		while len(g.get_choices()) > 0:
 			p = players[g.player - 1]
 			c = p.get_choice()
 			g.move(c, g.player)
-			# g.print_board()
+			if is_print:
+				g.print_board()
 			if g.is_winner(c, g.player):
 				print(p.name + " is WINNER!")
 				if p.player == 1:
